b8b84ad153c0738d70e899da9e19ce7598bdf968
Sankarshan Mudkavi <Sankarshan.Mudkavi@gmail.com>
2013-08-17 17:36:59 -0400

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ TwitterCloneApp::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.force_ssl = true
 end
