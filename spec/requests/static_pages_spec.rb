10192ec878ad1fb6cbc53ce0ecd82110fb608db9
Sankarshan Mudkavi <Sankarshan.Mudkavi@gmail.com>
2013-08-17 18:40:25 -0400

    Completed signin and signout

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,7 +20,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     page.should have_selector 'title', text: full_title('Sign up')
-    click_link "sample app"
+    click_link "Twitter clone"
     page.should have_selector 'title', text: full_title('')
   end
 
